b0e73760d2332e56b5a133cbbfb996e68af828b6
project-dy <kero122899@gmail.com>
2025-02-21 23:01:04 +0900

    bg

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -7,38 +7,73 @@ import { useTemplateRef } from "vue";
 
 const background = useTemplateRef("background");
 
-// Asynchronous IIFE
-(async () => {
-  // Create a PixiJS application.
-  const app = new Application();
+class PIXIBackground {
+  private app = new Application();
+  // private textureUrl: string;
+  private albumArt: Sprite = new Sprite();
+  public inited: Boolean = false;
+  constructor() {
+    //textureUrl: string) {
+    // this.textureUrl = textureUrl;
+    this.init();
+  }
+  async init() {
+    // await this.app.init({
+    //   resizeTo: window,
+    // });
+    // Intialize the application.
+    await this.app.init({
+      resizeTo: window,
+      // width: window.innerWidth,
+      // height: window.innerHeight,
+    });
 
-  // Intialize the application.
-  await app.init({
-    resizeTo: window,
-    // width: window.innerWidth,
-    // height: window.innerHeight,
-  });
+    // Then adding the application's canvas to the DOM body.
+    // document.body.appendChild(app.canvas);
+    background.value?.appendChild(this.app.canvas);
 
-  // Then adding the application's canvas to the DOM body.
-  // document.body.appendChild(app.canvas);
-  background.value?.appendChild(app.canvas);
+    // Load the albumart texture.
+    // const texture = await Assets.load(this.textureUrl);
 
-  // Load the bunny texture.
-  const texture = await Assets.load("/public/temp.jpg");
+    // // Create a new Sprite from an image path
+    this.albumArt = new Sprite();
+    // this.albumArt.texture = texture;
+    // this.albumArt.texture.update();
 
-  // Create a new Sprite from an image path
-  const albumArt = new Sprite(texture);
+    // Add to stage
+    this.app.stage.addChild(this.albumArt);
 
-  // Add to stage
-  app.stage.addChild(albumArt);
+    // Center the sprite's anchor point
+    this.albumArt.anchor.set(0.5);
 
-  // Center the sprite's anchor point
-  albumArt.anchor.set(0.5);
-
-  // Move the sprite to the center of the screen
-  albumArt.x = app.screen.width / 2;
-  albumArt.y = app.screen.height / 2;
-})();
+    // Move the sprite to the center of the screen
+    this.albumArt.x = this.app.screen.width / 2;
+    this.albumArt.y = this.app.screen.height / 2;
+    this.inited = true;
+  }
+  public async updateTexture(textureUrl: string): Promise<void> {
+    // console.log(bg.inited);
+    // if (!bg.inited)
+    //   return new Promise(() => setTimeout(this.updateTexture, 100, textureUrl))
+    //     .then(() => Promise.resolve(this.inited))
+    //     .then(() => this.updateTexture(textureUrl));
+    this.albumArt.texture = await Assets.load(textureUrl);
+    this.albumArt.texture.update();
+    console.log(this.albumArt.texture.source);
+    console.log(this.albumArt.texture.source.label.includes(textureUrl));
+  }
+}
+// const bg = new PIXIBackground("/public/temp.jpg");
+const bg = new PIXIBackground();
+async function updateBackgroundImage(textureURL: string) {
+  console.log(bg.inited);
+  // await bg.updateTexture(textureURL);
+  // console.log("r");
+  setTimeout(() => bg.updateTexture(textureURL), 1000);
+  console.log("r");
+}
+updateBackgroundImage("/public/temp1.jpg");
+defineExpose({ updateBackground: updateBackgroundImage });
 </script>
 
 <template>
